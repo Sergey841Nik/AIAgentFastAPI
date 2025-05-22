41e111fb278431063dee2a913f7856a8537ab613
Сергей <nikitos841@yandex.ru>
2025-05-22 21:45:56 +0400

    add create chroma db

--- src/chroma_db/chroma_storage.py
+++ src/chroma_db/chroma_storage.py
@@ -1,5 +1,7 @@
 from logging import Logger
+import time
 from typing import Literal
+from pathlib import Path
 
 from langchain_core.documents import Document
 import torch
@@ -7,6 +9,7 @@ from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 
 from src.config import BASE_DIR, settings, setup_logger
+from src.document.utils import document_generator
 
 logger: Logger = setup_logger(__name__) 
 
@@ -19,12 +22,52 @@ class ChromaVectorStorage:
         self.name_vector_storage = name_vector_storage
         self._store: Chroma | None = None
 
-    def create(self):
+    def create(
+            self,
+            folder_path: Path,
+            persist_dir: str,
+            embedding_model: str = settings.LM_MODEL_NAME
+        ) -> Chroma:
         """
-        Создает хранилище векторов в базе данных Chroma.
-        """
-        pass
-                
+    Создает ChromaDB из всех файлов в папке
+    
+    :param folder_path: Путь к папке с файлами
+    :param persist_dir: Директория для сохранения БД
+    :param embedding_model: Модель для эмбеддингов
+    :return: Объект ChromaDB
+    """
+        try:
+            start_time: float = time.time()
+            logger.info("Загрузка модели эмбеддингов...")
+            # Определяем устройство для вычислений: GPU если доступен, иначе CPU
+            device: Literal['cuda'] | Literal['cpu'] = "cuda" if torch.cuda.is_available() else "cpu"
+            embeddings = HuggingFaceEmbeddings(
+                    model_name=embedding_model,
+                    model_kwargs={"device": device},
+                    encode_kwargs={"normalize_embeddings": True},
+                )
+            logger.info("Модель загружена за %.2f сек", time.time() - start_time)
+
+            documents = list(document_generator(folder_path))
+
+            logger.info("Количество документов: %d", len(documents))
+            ids: list[str] = [f"doc_{i}" for i in range(len(documents))]
+
+            logger.info("Создание базы данных Chroma...")
+            db = Chroma.from_documents(
+                documents=documents,
+                embedding=embeddings,
+                persist_directory=persist_dir,
+                collection_name=self.name_vector_storage,
+                ids=ids
+            )
+            logger.info("База данных создана за %.2f сек", time.time() - start_time)
+            
+            return db
+        except Exception as e:
+            logger.error("Ошибка при создании базы данных Chroma: %s", str(e))
+            raise
+                    
 
     async def init(self):
         """
