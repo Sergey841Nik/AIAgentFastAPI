c140bcd94790cd730c22b8fa64c01329d59d620e
Сергей <nikitos841@yandex.ru>
2025-05-25 20:11:06 +0400

    add endpoint ask

--- src/api/views.py
+++ src/api/views.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 
-from fastapi import APIRouter, UploadFile, File
+from fastapi import APIRouter, UploadFile, File, Depends
 
 from src.api.upload import upload_files
-from src.api.schemas import UploadFileSchemas
+from src.api.schemas import UploadFileSchemas, AskResponse
 from src.config import BASE_DIR
+from src.chroma_db.chroma_storage import ChromaVectorStorage
 
 router = APIRouter(prefix="/files", tags=["files"])
 
@@ -12,3 +13,21 @@ router = APIRouter(prefix="/files", tags=["files"])
 async def files_upload(files: list[UploadFile] = File(...)) -> list[UploadFile]:
     upload_dir: Path = BASE_DIR / "upload_files" / "test"
     return await upload_files(files, upload_dir)
+
+@router.post("/ask")
+async def ask(
+    query: AskResponse,
+):
+    vectorstore = ChromaVectorStorage(name_vector_storage="my_collection_test")
+    await vectorstore.init()
+    results = await vectorstore.asimilarity_search(
+        query=query.response, with_score=True, k=5
+    )
+    formatted_results = []
+    for doc, score in results:
+        formatted_results.append({
+            "text": doc.page_content,
+            "metadata": doc.metadata,
+            "similarity_score": score,
+        })
+    return {"results": formatted_results}
