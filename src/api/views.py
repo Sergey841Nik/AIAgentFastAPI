54479786ecc545d442dcb43850b0df9a0b45fa6c
Сергей <nikitos841@yandex.ru>
2025-05-26 23:08:49 +0400

    add endpoint create vector db

--- src/api/views.py
+++ src/api/views.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from fastapi import APIRouter, UploadFile, File, Depends
+from fastapi import APIRouter, UploadFile, File
 
 from src.api.upload import upload_files
 from src.api.schemas import UploadFileSchemas, AskResponse
@@ -9,10 +9,16 @@ from src.chroma_db.chroma_storage import ChromaVectorStorage
 
 router = APIRouter(prefix="/files", tags=["files"])
 
-@router.post("/upload", response_model=list[UploadFileSchemas])
-async def files_upload(files: list[UploadFile] = File(...)) -> list[UploadFile]:
+@router.post("/upload")
+async def files_upload(files: list[UploadFile] = File(...)) -> list[UploadFileSchemas]:
     upload_dir: Path = BASE_DIR / "upload_files" / "test"
     return await upload_files(files, upload_dir)
+
+@router.post("/create_vector_storage")
+def create_vector_storage() -> dict[str, str]:
+    vector_storage = ChromaVectorStorage(name_vector_storage="my_collection_test")
+    vector_storage.create(folder_path=Path(BASE_DIR / "upload_files" / "test"), persist_dir="src/chroma_db/vector_db")
+    return {"message": "Векторное хранилище создано"}
 
 @router.post("/ask")
 async def ask(
@@ -23,7 +29,8 @@ async def ask(
     results = await vectorstore.asimilarity_search(
         query=query.response, with_score=True, k=5
     )
-    formatted_results = []
+    formatted_results: list = []
+
     for doc, score in results:
         formatted_results.append({
             "text": doc.page_content,
