37cb6b025183ad033015926e814b3431c995a0f6
Сергей <nikitos841@yandex.ru>
2025-05-26 23:11:07 +0400

    fixed bug in upload.py

--- src/api/upload.py
+++ src/api/upload.py
@@ -3,33 +3,34 @@ from pathlib import Path
 
 from src.api.schemas import UploadFileSchemas
 
+
 async def upload_files(
-        files: list[UploadFile],
-        upload_dir: Path,
-) -> list[UploadFile]:
-    saved_files = []
+    files: list[UploadFile],
+    upload_dir: Path,
+) -> list[UploadFileSchemas]:
+    saved_files: list = []
     upload_dir.mkdir(parents=True, exist_ok=True)
 
     for file in files:
         try:
             # Создаем путь для сохранения файла
             file_path = upload_dir / file.filename
-            
+
             # Сохраняем файл
             with file_path.open("wb") as buffer:
                 buffer.write(await file.read())
-            
+
             saved_files.append(
                 UploadFileSchemas(
                     filename=file.filename,
                     content_type=file.content_type,
-                    size=file_path.stat().st_size
+                    size=file_path.stat().st_size,
                 )
             )
         except Exception as e:
             raise HTTPException(
                 status_code=500,
-                detail=f"Ошибка при загрузке файла {file.filename}: {str(e)}"
+                detail=f"Ошибка при загрузке файла {file.filename}: {str(e)}",
             )
-    
-    return  saved_files
+
+    return saved_files
