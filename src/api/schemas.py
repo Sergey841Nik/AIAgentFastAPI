5f9c5437a481afe0658741a345b1c2f7eaa83085
Сергей <nikitos841@yandex.ru>
2025-06-01 16:24:17 +0400

    add schemas for auth

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field, ConfigDict
 
 class UploadFileSchemas(BaseModel):
     filename: str
@@ -7,3 +7,15 @@ class UploadFileSchemas(BaseModel):
 
 class AskResponse(BaseModel):
     response: str
+
+class EmailModel(BaseModel):
+    email: EmailStr = Field(description="Электронная почта")
+    model_config = ConfigDict(from_attributes=True)
+
+class UserAddDB(EmailModel):
+    password: bytes = Field(description="Пароль в формате HASH-строки")
+
+class UserAuth(EmailModel):
+    password: str = Field(
+        min_length=5, max_length=50, description="Пароль, от 5 до 50 знаков"
+    )
